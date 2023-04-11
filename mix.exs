f61b37ec3a9387c2a844c0461e9b397be6543b36
João Pedro Alves <jotalmeida007@hotmail.com>
2023-04-11 13:25:27 -0300

    chore: update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCnab.MixProject do
   def project do
     [
       app: :ex_cnab,
-      version: "0.5.1",
+      version: "1.0.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
